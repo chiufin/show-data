ed2d5884a77eebd64aff8da5d7d47d623889023c
stacy <chiufin@hotmail.com>
2019-10-09 00:08:23 +0100

    pagination works

--- client/src/features/pagination/paginationSlice.js
+++ client/src/features/pagination/paginationSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from 'redux-starter-kit'
 
 export const PaginationFilters = {
+  INIT: 'INIT',
   PREVIOUS: 'PREVIOUS',
   NEXT: 'NEXT'
 }
@@ -10,12 +11,21 @@ const paginationSlice = createSlice({
   initialState: {
     start: 0,
     amount: 20,
-    total: 500
+    total: 500,
   },
   reducers: {
     changePage(state, action) {
-      console.log(action.payload)
-      return action.payload
+      switch(action.payload){
+        case PaginationFilters.NEXT:
+          state.start += state.amount
+          break;
+        case PaginationFilters.PREVIOUS: 
+          if(state.start > 0){
+            state.start -= state.amount
+          }
+          break;
+        default:
+      }
     }
   }
 })
