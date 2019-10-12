2d3b3c005b1f1e42ba1ee8cd769c1441035e3cda
stacy <chiufin@hotmail.com>
2019-10-12 23:18:52 +0100

    init pie

--- client/src/components/ChartPage.js
+++ client/src/components/ChartPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Bar from '../features/chart/Bar'
+import Pie from '../features/chart/Pie'
 
 const ChartPage = () => {
     return (
         <div>
             <h2>Chart</h2>
             <Bar />
+            <Pie />
         </div>
     );
 }
