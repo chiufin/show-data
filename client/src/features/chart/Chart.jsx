27edcf70de2d0a35356a56f41349a7195ff48c48
stacy <chiufin@hotmail.com>
2019-10-12 22:44:51 +0100

    use DOB to calcute the amount of month

--- client/src/features/chart/Chart.jsx
+++ client/src/features/chart/Chart.jsx
@@ -4,52 +4,47 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'rech
 import { useDispatch } from 'react-redux'
 import { fetchMockData } from '../grid/gridSlice'
 
-
-const Chart = () => {  
+const Chart = ({list}) => {  
     const dispatch = useDispatch()
 
     useEffect(() => {
       dispatch(fetchMockData())
     }, [dispatch])
-  
-    const data = [
-        {
-          name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-        },
-        {
-          name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-        },
-        {
-          name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-        },
-        {
-          name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-        },
-        {
-          name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-        },
-        {
-          name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-        },
-        {
-          name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-        },
-      ];
-      
+
+    const parseData = list => list.reduce((acc, current) => {
+        let parseDOB = current['date_of_birth'].split('/');
+        let thisMonth = Number(parseDOB[1])
+        if(!acc[thisMonth]){
+            acc[thisMonth] = 1
+        }else{
+            acc[thisMonth] = acc[thisMonth]+1
+        }
+        return acc
+    }, {})
+
+    const sumOfMonth = parseData(list)
+    const monthList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    let data = []
+    monthList.forEach((month, i)=> {
+        data.push({
+            name: month,
+            amount: sumOfMonth[i+1]
+        })
+    })
+    
     return (
       <div>
         <BarChart
             width={500}
             height={300}
             data={data}
-            margin={{ top: 5, right: 30, left: 20, bottom: 5}}>
+            >
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="name" />
             <YAxis />
             <Tooltip />
             <Legend />
-            <Bar dataKey="pv" fill="#8884d8" />
-            <Bar dataKey="uv" fill="#82ca9d" />
+            <Bar dataKey="amount" fill="#8884d8" />
         </BarChart>
       </div>
     )
