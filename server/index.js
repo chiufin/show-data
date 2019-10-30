89c317ff02202237b946350a4ca948502d3541f3
stacy <chiufin@hotmail.com>
2019-10-30 23:53:55 +0000

    prettier server files

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-var express = require('express')
-var cors = require('cors')
-var data = require('./MOCK_DATA.json')
+var express = require("express");
+var cors = require("cors");
+var data = require("./MOCK_DATA.json");
 
-var app = express()
-app.use(cors())
+var app = express();
+app.use(cors());
 
-app.get('/', function (req, res) {
-  res.status(200).json(data)
-})
+app.get("/", function(req, res) {
+  res.status(200).json(data);
+});
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!')
-})
+app.listen(5000, function() {
+  console.log("Example app listening on port 5000!");
+});
